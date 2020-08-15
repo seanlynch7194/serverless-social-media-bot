6dc00aef7d2c125b4e3b896484d21b34985d87ec
Sean Lynch <sean@webdoc.digital>
2020-08-15 22:03:22 +0100

    Implement Twitter service
    client to interface with Twitter

--- src/Bot/Posts/Framework/PostsServiceProvider/index.ts
+++ src/Bot/Posts/Framework/PostsServiceProvider/index.ts
@@ -1,9 +1,20 @@
 import { bind } from '../../../../App/Container';
 import DynamoRepository from '../../Infrastructure/DynamoRepository';
+import Twitter from '../../Infrastructure/Twitter';
+import config from '../../../../App/Config';
 
 const PostsServiceProvider = () => {
     bind('PostsRepository', () => {
         return DynamoRepository();
+    });
+
+    bind('Twitter', () => {
+        return Twitter({
+            consumerKey: config('twitter.consumerKey'),
+            consumerSecret: config('twitter.consumerSecret'),
+            accessToken: config('twitter.accessToken'),
+            accessTokenSecret: config('twitter.accessTokenSecret'),
+        });
     });
 }
 
--- src/Bot/Posts/Infrastructure/Twitter/index.ts
+++ src/Bot/Posts/Infrastructure/Twitter/index.ts
@@ -1,12 +1,40 @@
 import { SocialNetwork } from '../../Domain/SocialNetwork';
-import { Post } from '../../Domain/Post';
 import { TwitterPost } from '../../Domain/TwitterPost';
+import TwitterClient from 'twitter-lite';
 
-const Twitter = (): SocialNetwork => {
+type TwitterConfig = {
+    consumerKey: string,
+    consumerSecret: string,
+    accessToken: string,
+    accessTokenSecret: string,
+};
 
+const Twitter = (config: TwitterConfig): SocialNetwork => {
+    console.log('running twitter');
+
+    const client = new TwitterClient({
+        consumer_key: config.consumerKey,
+        consumer_secret: config.consumerSecret,
+        access_token_key: config.accessToken,
+        access_token_secret: config.accessTokenSecret,
+    });
+      
     return {
         publish: (post: TwitterPost) => {
             return Promise.resolve();
+        },
+
+        testConnection: () => {
+            console.log('testing connections');
+            return client
+                .get("account/verify_credentials")
+                .then(results => {
+                    console.log("results", results);
+                })
+                .catch((err) => {
+                    console.log('error');
+                    console.error(err);
+                });
         }
     };
 }
