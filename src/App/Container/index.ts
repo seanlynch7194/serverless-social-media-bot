f8e234af74b19490ce8712ce0d58a2698910e801
Sean Lynch <sean@webdoc.digital>
2020-08-16 18:49:56 +0100

    Throw error in Container if dependency is not registered
    Helps with debugging

--- src/App/Container/index.ts
+++ src/App/Container/index.ts
@@ -1,9 +1,13 @@
-let providers = {};
+let providers:any = {};
 
 export const bind = (name: string, factory: Function) => {
     providers[name] = factory;
 }
 
 export const resolve = (name: string) => {
+    if (!providers[name]) {
+        throw new Error(`${name} is not bound to the container`);
+    }
+
     return providers[name]();
 }
