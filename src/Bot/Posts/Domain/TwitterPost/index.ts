88400d8ac89f1c125fca801c3f8ac2c581325b30
Sean Lynch <sean@webdoc.digital>
2020-08-19 20:57:50 +0100

    Implement toObject method on TwitterPost
    So it can be cast to primitives for persistence

--- src/Bot/Posts/Domain/TwitterPost/index.ts
+++ src/Bot/Posts/Domain/TwitterPost/index.ts
@@ -1,16 +1,26 @@
 import MakePostId, { PostId } from '../PostId';
 import MakeTwitterPostContent, { TwitterPostContent } from '../TwitterPostContent';
 import { PostPrimitiveObject, Post } from '../Post';
+import { CrossPostId, MakeCrossPostId } from '../CrossPostId';
 
 export type TwitterPost = Post;
 
-const MakeTwitterPost = (id: PostId, content: TwitterPostContent, images: Array<string>, crossPostId: string): TwitterPost => {
+const MakeTwitterPost = (id: PostId, content: TwitterPostContent, images: Array<string>, crossPostId: CrossPostId): TwitterPost => {
     return {
         getId: () => id,
         getContent: () => content,
         getType: () => 'twitter',
         getImages: () => images,
         getCrossPostId: () => crossPostId,
+        toObject: () => {
+            return {
+                id: id.getValue(),
+                content: content.getValue(),
+                type: 'twitter',
+                images: images,
+                crossPostId: crossPostId.getValue(),
+            };
+        }
     }
 }
 
@@ -19,7 +29,7 @@ export const MakeTwitterPostFromObject = (data: PostPrimitiveObject): TwitterPos
         MakePostId(data.id), 
         MakeTwitterPostContent(data.content),
         data.images,
-        data.crossPostId,
+        MakeCrossPostId(data.crossPostId),
     );
 } 
 
