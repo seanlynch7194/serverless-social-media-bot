6bdb5a9737b6d4b22ee80bdae1d0e8cb25fb8dad
Sean Lynch <sean@webdoc.digital>
2020-08-19 21:00:00 +0100

    Add getCrossPostBatch to Dynamo posts repository

--- src/Bot/Posts/Infrastructure/DynamoRepository/index.ts
+++ src/Bot/Posts/Infrastructure/DynamoRepository/index.ts
@@ -1,12 +1,22 @@
 import PostsRepository from '../../Domain/PostsRepository';
 import MakePostId, { PostId } from '../../Domain/PostId';
-import MakePost from '../../Domain/Post';
+import { MakeTwitterPostFromObject } from '../../Domain/TwitterPost';
 
 const DynamoRepository = (): PostsRepository => {
     return {
         getPost: () => {
-            const post = MakePost(MakePostId('xxx'), 'Lorem ipsum');
+            const post = MakeTwitterPostFromObject({
+                id: 'xxx', 
+                content: 'Lorem ipsum',
+                type: 'twitter',
+                images: [],
+                crossPostId: '',
+            });
             return Promise.resolve(post);
+        },
+
+        getCrossPostBatch: () => {
+            return Promise.resolve([]);
         },
 
         addPost: () => {
