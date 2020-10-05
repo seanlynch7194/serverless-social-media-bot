30153fbb9972f2f9c056ed2278a688ddc8aaab30
Sean Lynch <sean@webdoc.digital>
2020-10-05 21:41:09 +0100

    Create SocialNetwork wrapper
    So many social networks can be registered and all called through a single api

---
+++ src/Bot/Posts/Domain/SocialNetworks/index.ts
@@ -0,0 +1,36 @@
+import { SocialNetwork } from '../SocialNetwork';
+import { Post } from '../Post';
+
+type NetworkList = Array<SocialNetwork>;
+
+/**
+ * Create function that accepts a callback and maps over all networks
+ * 
+ * @param networks 
+ */
+const networkMap = (networks: NetworkList): Function => {
+    return (callback: Function) => Promise.all(networks.map((network: SocialNetwork) => callback(network)))
+}
+
+/**
+ * Social network wrapper that accepts an array of social networks
+ * and executes methods on each network.
+ * 
+ */
+export default (networks: NetworkList): SocialNetwork => {
+    const mapNetworks = networkMap(networks);
+
+    return {
+        publish: (post: Post) => {
+            return mapNetworks((network: SocialNetwork) => {
+                return network.publish(post);
+            });
+        },
+
+        testConnection: () => {
+            return mapNetworks((network: SocialNetwork) => {
+                return network.testConnection();
+            });
+        }
+    };
+}
