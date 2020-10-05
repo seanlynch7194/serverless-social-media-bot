64bdf41fb76dc181fc18e7d2bdd93a26f946f164
Sean Lynch <sean@webdoc.digital>
2020-10-05 22:20:57 +0100

    throw invalidPostId if PostId is too long or too short
    them the rules

--- src/Bot/Posts/Domain/PostId/index.ts
+++ src/Bot/Posts/Domain/PostId/index.ts
@@ -1,8 +1,23 @@
+import InvalidPostId from '../Exceptions/InvalidPostId';
+
 export type PostId = {
     getValue: () => string
 }
 
+/**
+ * Make PostId value object
+ * 
+ * @param id {string}
+ */
 const MakePostId = (id: string): PostId => {
+    if (id.length > 255) {
+        throw InvalidPostId.tooLong();
+    }
+
+    if (id.length <= 0) {
+        throw InvalidPostId.tooShort();
+    }
+
     return {
         getValue: (): string => id,
     }
