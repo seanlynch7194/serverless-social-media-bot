8f05c190ee83e6e2f7179474db99f1ee2f4eb931
Sean Lynch <sean@webdoc.digital>
2020-10-05 21:41:36 +0100

    Register social networks in Posts domain

--- src/Bot/Posts/Framework/PostsServiceProvider/index.ts
+++ src/Bot/Posts/Framework/PostsServiceProvider/index.ts
@@ -6,6 +6,8 @@ import config from '../../../../App/Config';
 import PostRandomToAllNetworks from '../../Application/Commands/PostRandomToAllNetworks';
 import GetCrossPostCollection from '../../Application/Queries/GetCrossPostCollection';
 import env from '../../../../App/env';
+import SocialNetworks from '../../Domain/SocialNetworks';
+import MockSocialNetwork from '../../Infrastructure/MockSocialNetwork';
 
 const PostsServiceProvider = () => {
     bind('PostsRepository', () => {
@@ -16,15 +18,7 @@ const PostsServiceProvider = () => {
         return DynamoRepository();
     });
 
-    bind('Twitter', () => {
-        return Twitter({
-            consumerKey: config('twitter.consumerKey'),
-            consumerSecret: config('twitter.consumerSecret'),
-            accessToken: config('twitter.accessToken'),
-            accessTokenSecret: config('twitter.accessTokenSecret'),
-        });
-    });
-
+    registerSocialNetworks();
     registerCommands();
     registerQueries();
 }
@@ -39,6 +33,30 @@ const registerQueries = () => {
     bind('GetCrossPostCollection', () => {
         return GetCrossPostCollection(resolve('PostsRepository'))
     })
+}
+
+/**
+ * Register Social Network wrapper that will proxy many
+ * social networks through a single api.
+ *  
+ */
+const registerSocialNetworks = () => {
+    bind('socialNetworks', () => {
+        if (env('NODE_ENV') === 'local') {
+            return SocialNetworks([MockSocialNetwork()]);
+        }
+
+        return SocialNetworks([resolve('Twitter')]);
+    });
+
+    bind('Twitter', () => {
+        return Twitter({
+            consumerKey: config('twitter.consumerKey'),
+            consumerSecret: config('twitter.consumerSecret'),
+            accessToken: config('twitter.accessToken'),
+            accessTokenSecret: config('twitter.accessTokenSecret'),
+        });
+    });
 }
 
 export default PostsServiceProvider;
