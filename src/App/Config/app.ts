fe8fb7691660003ac94576712f38f1dbf26363cd
Sean Lynch <sean@webdoc.digital>
2020-10-17 12:05:40 +0100

    add posts table name to app config

--- src/App/Config/app.ts
+++ src/App/Config/app.ts
@@ -1,7 +1,10 @@
 import PostsServiceProvider from '../../Bot/Posts/Framework/PostsServiceProvider';
+import env from '../env';
 
 export default {
     providers: [
         PostsServiceProvider,
     ],
+
+    postsTable: env('POSTS_TABLE_NAME'),
 };
