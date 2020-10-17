df452957ce112962ae20f557eec871927e9187f2
Sean Lynch <sean@webdoc.digital>
2020-10-17 12:05:07 +0100

    Install Serverless package 📦
    So we can use the Serverless Dynamo definition in Jest instead of duplicating the database properties

--- jest-dynamodb-config.js
+++ jest-dynamodb-config.js
@@ -1,35 +1,28 @@
 /**
  * @see https://jestjs.io/docs/en/dynamodb#use-jest-dynamodb-preset
  */
-module.exports = {
-    tables: [
-        {
-            TableName: `Local-Posts`,
-            KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
-            AttributeDefinitions: [{
-                AttributeName: 'id', AttributeType: 'S'
-            }, {
-                AttributeName: 'crossPostId', AttributeType: 'S'
-            }],
-            GlobalSecondaryIndexes: [{
-                IndexName: 'crossPostId',
-                KeySchema: [{
-                    AttributeName: 'crossPostId',
-                    KeyType: 'HASH'
-                }],
-                Projection: {
-                    ProjectionType: 'ALL'
-                },
-                ProvisionedThroughput: {
-                    ReadCapacityUnits: 1,
-                    WriteCapacityUnits: 1
-                },
-            }],
-            ProvisionedThroughput: { 
-                ReadCapacityUnits: 1, 
-                WriteCapacityUnits: 1
-            },
-        },
-    ],
-    port: 8000,
+module.exports = async () => {
+
+    /**
+     * @see https://github.com/shelfio/jest-dynamodb#22-examples
+     */
+    const serverless = new (require('serverless'))();
+    
+    await serverless.init();
+    const service = await serverless.variables.populateService({
+        region: 'eu-west-1',
+        env: 'local',
+    });
+
+    const resources = service.resources.Resources;
+  
+    const tables = Object.keys(resources)
+        .map(name => resources[name])
+        .filter(r => r.Type === 'AWS::DynamoDB::Table')
+        .map(r => r.Properties);
+  
+    return {
+        tables,
+        port: 8000
+    }
 };
