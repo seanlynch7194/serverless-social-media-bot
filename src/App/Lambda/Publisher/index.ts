a4ead0d23710ab38b744b8ed1cfa0e1b10b3cec4
Sean Lynch <sean@webdoc.digital>
2020-10-18 15:04:25 +0100

    Remove unused import

--- src/App/Lambda/Publisher/index.ts
+++ src/App/Lambda/Publisher/index.ts
@@ -1,5 +1,4 @@
 import { resolve } from '../../Container';
-import { SocialNetwork } from '../../../Bot/Posts/Domain/SocialNetwork';
 
 const Publisher = async (): Promise<any> => {
     const PublishNextToAllNetworks = resolve('PublishNextToAllNetworks');
