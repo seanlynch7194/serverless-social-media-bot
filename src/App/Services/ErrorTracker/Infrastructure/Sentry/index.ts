a17b47a2faa26514f63133d7b4307aa0fa1d688c
Sean Lynch <sean@webdoc.digital>
2020-11-07 22:44:09 +0000

    Implement and test Sentry

---
+++ src/App/Services/ErrorTracker/Infrastructure/Sentry/index.ts
@@ -0,0 +1,24 @@
+import ErrorTracker from '../../Domain/ErrorTracker';
+import { Environment } from '../../../../../Bot/Shared/Domain/Environment';
+const SentryIO = require("@sentry/serverless");
+
+const Sentry = (dsn: string, environment: Environment): ErrorTracker => {
+    SentryIO.AWSLambda.init({
+        dsn,
+        environment,
+        debug: (environment === 'local' ? true : false),
+    });
+
+    return {
+        handler: (handler) => {
+            return SentryIO.AWSLambda.wrapHandler(handler);
+        },
+
+        captureException: (err: Error): Promise<void> => {
+            SentryIO.captureException(err);
+            return Promise.resolve();
+        }
+    };
+}
+
+export default Sentry;
