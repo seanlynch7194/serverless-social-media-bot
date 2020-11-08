8495f9269d2e116f1aa769bf7c90d315df898ca1
Sean Lynch <sean@webdoc.digital>
2020-11-08 02:59:26 +0000

    Fix bug where mockSocialNetwork would output an unreadable object

--- src/Bot/Posts/Infrastructure/MockSocialNetwork/index.ts
+++ src/Bot/Posts/Infrastructure/MockSocialNetwork/index.ts
@@ -4,7 +4,7 @@ import { Post } from '../../Domain/Post';
 export default (): SocialNetwork => {
     return {
         publish: (post: Post) => {
-            console.log(`Published post: ${post.getId()}`);
+            console.log(`Published post: ${post.getId().getValue()}`);
             return Promise.resolve();
         },
     }
