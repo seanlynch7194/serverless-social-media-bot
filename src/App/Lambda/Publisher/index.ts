0019bc42c95bf8b5b17cd3c3df102b64719a43e5
Sean Lynch <sean@webdoc.digital>
2020-11-08 00:18:29 +0000

    Document Publisher function

--- src/App/Lambda/Publisher/index.ts
+++ src/App/Lambda/Publisher/index.ts
@@ -1,6 +1,14 @@
 import { resolve } from '../../Container';
+import { Context } from 'aws-lambda';
 
-const Publisher = async (): Promise<any> => {
+type PublisherEvent = {};
+
+/**
+ * 
+ * @param {any} event contains information from the invoker
+ * @param {Object} context contains information about the invocation, function, and execution environment.
+ */
+const Publisher = async (event: PublisherEvent, context: Context): Promise<any> => {
     const PublishNextToAllNetworks = resolve('PublishNextToAllNetworks');
 
     return PublishNextToAllNetworks();
