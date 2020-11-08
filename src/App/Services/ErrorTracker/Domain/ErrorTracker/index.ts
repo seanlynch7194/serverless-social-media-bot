5c686ed93ff2313a45d18821de316d67920f7e69
Sean Lynch <sean@webdoc.digital>
2020-11-08 01:59:01 +0000

    Add setContext method to ErrorTracker interface

--- src/App/Services/ErrorTracker/Domain/ErrorTracker/index.ts
+++ src/App/Services/ErrorTracker/Domain/ErrorTracker/index.ts
@@ -2,6 +2,8 @@ type ErrorTracker = {
     handler: (handler: Function) => Function;
 
     captureException: (err: Error) => Promise<void>
+
+    setContext: (key: string, value: any) => Promise<void>
 }
 
 export default ErrorTracker;
