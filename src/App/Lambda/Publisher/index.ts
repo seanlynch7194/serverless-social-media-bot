33f54e8900c11280943286eff8ac0af1c4900ffe
Sean Lynch <sean@webdoc.digital>
2020-11-08 00:38:56 +0000

    Fix type in docs

--- src/App/Lambda/Publisher/index.ts
+++ src/App/Lambda/Publisher/index.ts
@@ -5,8 +5,8 @@ type PublisherEvent = {};
 
 /**
  * 
- * @param {any} event contains information from the invoker
- * @param {Object} context contains information about the invocation, function, and execution environment.
+ * @param {PublisherEvent} event contains information from the invoker
+ * @param {Context} context contains information about the invocation, function, and execution environment.
  */
 const Publisher = async (event: PublisherEvent, context: Context): Promise<any> => {
     const PublishNextToAllNetworks = resolve('PublishNextToAllNetworks');
