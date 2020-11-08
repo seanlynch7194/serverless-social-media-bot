8945d5c59138b3686cf7780dfb28a00d1bc1091d
Sean Lynch <sean@webdoc.digital>
2020-11-08 03:02:03 +0000

    Add next posts to ErrorTracker when publishing
    So we know which posts were involved if there is an error. There may need to be manual clean up tasks with the post ids.

--- src/Bot/Posts/Application/Commands/PublishNextToAllNetworks/index.ts
+++ src/Bot/Posts/Application/Commands/PublishNextToAllNetworks/index.ts
@@ -2,8 +2,14 @@ import PostsRepository from '../../../Domain/PostsRepository';
 import { Post } from '../../../Domain/Post';
 import { SocialNetwork } from '../../../Domain/SocialNetwork';
 import { PostId } from '../../../Domain/PostId';
+import ErrorTracker from '../../../../../App/Services/ErrorTracker/Domain/ErrorTracker';
 
-const PublishNextToAllNetworks = (postsRepository: PostsRepository, socialNetworks: SocialNetwork) => {
+
+const PublishNextToAllNetworks = (
+    postsRepository: PostsRepository, 
+    socialNetworks: SocialNetwork,
+    errorTracker: ErrorTracker
+) => {
     return async () => {
         const nextPost = await postsRepository.getNextPost();
         if (!nextPost) {
@@ -12,6 +18,11 @@ const PublishNextToAllNetworks = (postsRepository: PostsRepository, socialNetwor
         }
 
         const posts = await postsRepository.getPostsByCrossPostId(nextPost.getCrossPostId());
+
+        errorTracker.setContext('nextPosts', {
+            nextPostId: nextPost.getId().getValue(),
+            posts: JSON.stringify(posts.map((post) => post.toObject())),
+        });
 
         return Promise.all(posts.map((post: Post) => {
             return socialNetworks.publish(post).then(() => {
