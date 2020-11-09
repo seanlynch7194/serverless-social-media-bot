df2d6990db28e60ec1b14e77a1731c73150cbba0
Sean Lynch <sean@webdoc.digital>
2020-11-09 00:09:03 +0000

    Remove debug code

--- scripts/debug/index.js
+++
@@ -1,5 +0,0 @@
-console.log(process.env);
-
-
-console.log('ptn_join', process.env.POSTS_TABLE_NAME.split('').join('.'));
-console.log('ter_join', process.env.TEST_ENV_REPLACEMENT.split('').join('.'));
